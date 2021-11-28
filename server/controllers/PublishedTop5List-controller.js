3af7c50e96e29034530f8d4ea5677545033d81e6
the20thtry <derekding28@gmail.com>
2021-11-28 17:31:50 -0500

    publishing lists works now

--- server/controllers/PublishedTop5List-controller.js
+++ server/controllers/PublishedTop5List-controller.js
@@ -1,4 +1,4 @@
-const Top5List = require('../models/publishedTop5List-model');
+const PublishedTop5List = require('../models/publishedTop5List-model');
 
 createTop5List = (req, res) => {
     const body = req.body;
@@ -11,7 +11,7 @@ createTop5List = (req, res) => {
         })
     }
 
-    const top5List = new Top5List(body);
+    const top5List = new PublishedTop5List(body);
     console.log("creating top5List: " + JSON.stringify(top5List));
     if (!top5List) {
         return res.status(400).json({ success: false, error: err })
@@ -19,6 +19,7 @@ createTop5List = (req, res) => {
     top5List
         .save() //something went wrong here
         .then(() => {
+            console.log("top5list successfully published!")
             return res.status(201).json({
                 success: true,
                 top5List: top5List,
@@ -32,4 +33,129 @@ createTop5List = (req, res) => {
                 message: 'Top 5 List Not Created!'
             })
         })
+}
+
+updateTop5List = async (req, res) => {
+    const body = req.body
+    console.log("updateTop5List: " + JSON.stringify(body));
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    PublishedTop5List.findOne({ _id: req.params.id }, (err, top5List) => {
+        console.log("top5List found: " + JSON.stringify(top5List));
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+
+        top5List.name = body.name
+        top5List.items = body.items
+        top5List.author=body.author
+        top5List.likes=body.likes
+        top5List.comments=body.comments
+        top5List.publishedDate=body.publishedDate
+        top5List.views=body.views
+
+        top5List
+            .save()
+            .then(() => {
+                console.log("SUCCESS!!!");
+                return res.status(200).json({
+                    success: true,
+                    id: top5List._id,
+                    message: 'Top 5 List updated!',
+                })
+            })
+            .catch(error => {
+                console.log("FAILURE: " + JSON.stringify(error));
+                return res.status(404).json({
+                    error,
+                    message: 'Top 5 List not updated!',
+                })
+            })
+    })
+}
+
+deleteTop5List = async (req, res) => {
+    PublishedTop5List.findById({ _id: req.params.id }, (err, top5List) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Top 5 List not found!',
+            })
+        }
+        PublishedTop5List.findOneAndDelete({ _id: req.params.id }, () => {
+            return res.status(200).json({ success: true, data: top5List })
+        }).catch(err => console.log(err))
+    })
+}
+
+getTop5ListById = async (req, res) => {
+    await PublishedTop5List.findById({ _id: req.params.id }, (err, list) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+        return res.status(200).json({ success: true, top5List: list })
+    }).catch(err => console.log(err))
+}
+getTop5Lists = async (req, res) => {
+    await PublishedTop5List.find({}, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Top 5 Lists not found` })
+        }
+        return res.status(200).json({ success: true, data: top5Lists })
+    }).catch(err => console.log(err))
+}
+getTop5ListPairs = async (req, res) => {
+    await PublishedTop5List.find({ }, (err, top5Lists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!top5Lists) {
+            console.log("!top5Lists.length");
+            return res
+                .status(404)
+                .json({ success: false, error: 'Top 5 Lists not found' })
+        }
+        else {
+            // PUT ALL THE LISTS INTO ID, NAME PAIRS
+            let pairs = [];
+            for (let key in top5Lists) {
+                let list = top5Lists[key];
+                let pair = {
+                    _id: list._id,
+                    name: list.name,
+                    author: list.author,
+                    items: list.items,
+                    likes: list.likes,
+                    comments: list.comments,
+                    publishedDate: list.publishedDate,
+                    views: list.views
+
+                };
+                pairs.push(pair);
+            }
+            return res.status(200).json({ success: true, idNamePairs: pairs })
+        }
+    }).catch(err => console.log(err))
+}
+
+module.exports = {
+    createTop5List,
+    updateTop5List,
+    deleteTop5List,
+    getTop5Lists,
+    getTop5ListPairs,
+    getTop5ListById
 }
